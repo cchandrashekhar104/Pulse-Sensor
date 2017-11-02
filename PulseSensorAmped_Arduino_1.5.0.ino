53ae2678fef9304c0efb980431f34b06e1a98a88
cchandrashekhar104 <cchandrashekhar104@gmail.com>
2017-11-02 11:18:14 +0530

    Add files via upload

---
+++ PulseSensorAmped_Arduino_1.5.0.ino
@@ -0,0 +1,123 @@
+
+/*  Pulse Sensor Amped 1.5    by Joel Murphy and Yury Gitman   http://www.pulsesensor.com
+
+----------------------  Notes ----------------------  ----------------------
+This code:
+1) Blinks an LED to User's Live Heartbeat   PIN 13
+2) Fades an LED to User's Live HeartBeat    PIN 5
+3) Determines BPM
+4) Prints All of the Above to Serial
+
+Read Me:
+https://github.com/WorldFamousElectronics/PulseSensor_Amped_Arduino/blob/master/README.md
+ ----------------------       ----------------------  ----------------------
+*/
+#include "ESP8266WiFi.h"
+#define PROCESSING_VISUALIZER 1
+#define SERIAL_PLOTTER  2
+#include <Ticker.h>
+
+const char server[] = "192.168.43.184"; 
+
+const char* MY_SSID = "g2";
+const char* MY_PWD =  "qwerty123";
+Ticker sender;
+WiFiClient client;
+//  Variables
+int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
+int blinkPin = 13;                // pin to blink led at each beat
+int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
+int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
+bool sendok=false;
+// Volatile Variables, used in the interrupt service routine!
+volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
+volatile int Signal;                // holds the incoming raw data
+volatile int IBI = 600;             // int that holds the time interval between beats! Must be seeded!
+volatile boolean Pulse = false;     // "True" when User's live heartbeat is detected. "False" when not a "live beat".
+volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
+
+// SET THE SERIAL OUTPUT TYPE TO YOUR NEEDS
+// PROCESSING_VISUALIZER works with Pulse Sensor Processing Visualizer
+//      https://github.com/WorldFamousElectronics/PulseSensor_Amped_Processing_Visualizer
+// SERIAL_PLOTTER outputs sensor data for viewing with the Arduino Serial Plotter
+//      run the Serial Plotter at 115200 baud: Tools/Serial Plotter or Command+L
+static int outputType = SERIAL_PLOTTER;
+
+
+void setup(){
+  //pinMode(blinkPin,OUTPUT);         // pin that will blink to your heartbeat!
+  //pinMode(fadePin,OUTPUT);          // pin that will fade to your heartbeat!
+  Serial.begin(115200);             // we agree to talk fast!
+  Serial.print("Connecting to "+*MY_SSID);
+  WiFi.begin(MY_SSID, MY_PWD);
+  Serial.println("going into wl connect");
+
+  while (WiFi.status() != WL_CONNECTED) //not connected,  ...waiting to connect
+    {
+      delay(1000);
+      Serial.print(".");
+    }
+  Serial.println("wl connected");
+  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS
+   // IF YOU ARE POWERING The Pulse Sensor AT VOLTAGE LESS THAN THE BOARD VOLTAGE,
+   // UN-COMMENT THE NEXT LINE AND APPLY THAT VOLTAGE TO THE A-REF PIN
+//   analogReference(EXTERNAL);
+  sender.attach(2, senderFunc);
+}
+
+
+//  Where the Magic Happens
+void loop(){
+
+    serialOutput() ;
+    if(sendok)
+    {
+            if (client.connect(server, 80)) {
+    //Serial.println("connected to server");
+    //WiFi.printDiag(Serial);
+     /*client.println("POST /heartBeat/ HTTP/1.1"); //change this if using your Sub-domain
+     client.print("Host: 192.168.43.184\n");                 //change this if using your Domain
+     client.println("User-Agent: ESP8266/1.0");
+     client.println("Connection: close"); 
+     client.println("Content-Type: application/x-www-form-urlencoded");
+     client.print("Content-Length: ");*/
+     String data="bpm=";
+     data+=BPM;
+     String msg="POST /heartBeat/ HTTP/1.1\nHost: 192.168.43.184\nUser-Agent: ESP8266/1.0\nConnection: close\nContent-Type: application/x-www-form-urlencoded\nContent-Length: ";
+     msg+=data.length();
+     msg+="\n\n";
+     msg+=data;
+     //client.print(data.length());
+     //client.print("\n\n");
+     //client.print(data);
+     client.print(msg);
+     client.stop(); 
+     }
+     sendok=false;
+    }
+
+  if (QS == true){     // A Heartbeat Was Found
+                       // BPM and IBI have been Determined
+                       // Quantified Self "QS" true when arduino finds a heartbeat
+        fadeRate = 255;         // Makes the LED Fade Effect Happen
+                                // Set 'fadeRate' Variable to 255 to fade LED with pulse
+        serialOutputWhenBeatHappens();   // A Beat Happened, Output that to serial.
+        QS = false;                      // reset the Quantified Self flag for next time
+  }
+
+  //ledFadeToBeat();                      // Makes the LED Fade Effect Happen
+  delay(20);                             //  take a break
+}
+
+
+
+void senderFunc()
+{
+  sendok=true;
+}
+
+void ledFadeToBeat(){
+    fadeRate -= 15;                         //  set LED fade value
+    fadeRate = constrain(fadeRate,0,255);   //  keep LED fade value from going into negative numbers!
+    analogWrite(fadePin,fadeRate);          //  fade LED
+  }
